8298e26c2420383b18e66cf78de90764c0219ba6
Juan José Cifuentes <jcifuentesc03@gmail.com>
2021-10-30 19:00:51 -0500

    Se creo la clase Dungeon

---
+++ src/Dungeon.java
@@ -0,0 +1,11 @@
+public class Dungeon {
+
+    String [] options;
+    String mainString;
+    Game rpg;
+
+    public Dungeon(String[] options,  Game rpg) {
+        this.options = options;
+        this.rpg = rpg;
+    }
+}
