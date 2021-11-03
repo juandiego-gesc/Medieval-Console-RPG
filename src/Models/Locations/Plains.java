fa5c1c116097d47fd3b8a9211ccb39035d094799
Juan Diego Garcia Escobar <juangares@unisabana.edu.co>
2021-11-02 21:58:27 -0500

    GUI sale pa pintura

--- src/Models/Locations/Plains.java
+++ src/Models/Locations/Plains.java
@@ -4,20 +4,24 @@ import Controllers.Game;
 
 public class Plains extends Location{
 
-    public Plains(String[] options,  Game rpg) { super(options, rpg); }
+    public Plains(String[] options,  Game rpg) {
+        super(options, rpg);
+        mainString = "A la verga wey, un lobo!";
+    }
 
     @Override
     void command1() {
-
+        System.out.println("Perro HP");
     }
 
     @Override
     void command2() {
-
+        System.out.println("Pues como dijo Andrea...");
     }
 
     @Override
     void command3() {
-
+        //volver
+        rpg.setActualPlace("Ciudad");
     }
 }
