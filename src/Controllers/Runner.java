1111db0cdb14454c50b90c22154c371a41d64c2e
Hector <hdiaz35120@gmail.com>
2021-11-05 16:38:06 -0500

    Final hector

--- src/Controllers/Runner.java
+++ src/Controllers/Runner.java
@@ -39,13 +39,13 @@ public class Runner {
         return temp;
     }
     
-    private static HashMap<Enemy, Boolean> setUpEnemies(Game rpg){
-        HashMap<Enemy,Boolean> temp = new HashMap<>();
-        temp.put(new Enemy(70, new int[]{5, 10},"Te has encontrado con un feroz lobo",rpg.items.get(3)),true);//lobo
-        temp.put(new Enemy(100, new int[]{5, 15},"Te has encontrado con un goblin \n Parece que esta furioso ¡ten cuidado!",rpg.items.get(4)),true);//goblin
-        temp.put(new Enemy(80, new int[]{10, 20},"Te has encontrado con un feroz lobo",rpg.items.get(3)),true);//lobo
-        temp.put(new Enemy(240, new int[]{15, 25},"Prueba esqueleto",rpg.items.get(4)),true);//Esqueleto
-        temp.put(new Enemy(300, new int[]{20, 30},"Prueba dragon",rpg.items.get(4)),true);//Dragon
+    private static HashMap<Integer, Enemy> setUpEnemies(Game rpg){
+        HashMap<Integer, Enemy> temp = new HashMap<>();
+        temp.put(1 ,new Enemy(70, new int[]{5, 10},"Te has encontrado con un feroz lobo numero 1",rpg.items.get(3)));//lobo1
+        temp.put(2 ,new Enemy(100, new int[]{5, 15},"Te has encontrado con un goblin \n Parece que esta furioso ¡ten cuidado!",rpg.items.get(4)));//goblin
+        temp.put(3 ,new Enemy(80, new int[]{10, 20},"Te has encontrado con un feroz lobo numero 2",rpg.items.get(3)));//lobo2
+        temp.put(4 ,new Enemy(240, new int[]{15, 25},"Te has encontrado un esqueleto",rpg.items.get(4)));//Esqueleto
+        temp.put(5 ,new Enemy(300, new int[]{20, 30},"Te has encontrado un dragon",rpg.items.get(4)));//Dragon
         return temp;
     }
 
@@ -70,15 +70,15 @@ public class Runner {
 
         temp.put("Bosque",new Forest(new String[]{"Atacar", "Avanzar", "Volver"}, rpg, enemies[0]));
 
-        temp.put("Llanuras",new Plains(new String[]{"Atacar", "Avanzar", "Volver"}, rpg, enemies[0], enemies[2]));
+        temp.put("Llanuras",new Plains(new String[]{"Atacar", "Avanzar", "Volver"}, rpg, rpg.enemies.get(1), rpg.enemies.get(3)));
 
         //En todos estos toca decir que enemigo hay, donde esta ocurriendo la pelea, si da vida extra, si cambia el estado del juego o si da recompensa e
 
-        temp.put("Combate1",new Combat(new String[]{"Atacar", "Huir", "Curarse"}, rpg,enemies[0],temp.get("Llanuras"),false,false,false)); //Lobo, Llanura, no, no, no
-        temp.put("Combate2",new Combat(new String[]{"Atacar", "Huir", "Curarse"}, rpg,enemies[1],temp.get("Bosque"),true,true,true)); //Goblin, Forest, si, si, si
-        temp.put("Combate3",new Combat(new String[]{"Atacar", "Huir", "Curarse"}, rpg,enemies[2],temp.get("Llanuras"),false,false,false)); //Lobo, Llanura, no, no, no
-        temp.put("Combate4",new Combat(new String[]{"Atacar", "Huir", "Curarse"}, rpg,enemies[3],temp.get("Mazmorra"),true,false,false)); //Esqueleto, Dungeon, si, no, no
-        temp.put("Combate5",new Combat(new String[]{"Atacar", "Huir", "Curarse"}, rpg,enemies[4],temp.get("Mazmorra"),false,true,false)); //Dragon, Dungeon, no, si, no
+        temp.put("Combate1",new Combat(new String[]{"Atacar", "Huir", "Curarse"}, rpg,rpg.enemies.get(1),temp.get("Llanuras"),false,false,false)); //Lobo, Llanura, no, no, no
+        temp.put("Combate2",new Combat(new String[]{"Atacar", "Huir", "Curarse"}, rpg,rpg.enemies.get(2),temp.get("Bosque"),true,true,true)); //Goblin, Forest, si, si, si
+        temp.put("Combate3",new Combat(new String[]{"Atacar", "Huir", "Curarse"}, rpg,rpg.enemies.get(3),temp.get("Llanuras"),false,false,false)); //Lobo, Llanura, no, no, no
+        temp.put("Combate4",new Combat(new String[]{"Atacar", "Huir", "Curarse"}, rpg,rpg.enemies.get(4),temp.get("Mazmorra"),true,false,false)); //Esqueleto, Dungeon, si, no, no
+        temp.put("Combate5",new Combat(new String[]{"Atacar", "Huir", "Curarse"}, rpg,rpg.enemies.get(5),temp.get("Mazmorra"),false,true,false)); //Dragon, Dungeon, no, si, no
 
 
         temp.put("Tablero",new QuestBoard(new String[]{"Goblin", "Dragon", "Volver"}, rpg));
